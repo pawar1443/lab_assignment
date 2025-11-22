b951f1b1537e46b511f78b7faf44e0bbac845bb2
pawar1443 <pawarbirai@gmail.com>
2025-11-22 16:25:21 +0530

    added palindrome

--- Q.1/Q1_j.py
+++ Q.1/Q1_j.py
@@ -1,2 +1,10 @@
-name = input("enter a name : ")
-print(name.index(2))
+def is_palindrome(s):
+    s = s.replace(" ", "").lower()
+    return s == s[::-1]
+
+string = input("Enter a string: ")
+
+if is_palindrome(string):
+    print("It is a Palindrome")
+else:
+    print("It is NOT a Palindrome")
